a625488356f15d58317a571618f42fadb99744a4
Lawrence Hsieh <lawrencehsieh01410901database@gmail.com>
2025-07-16 16:28:30 +0800

    Delete Py_testing and Modify Py_20250716.py

--- Py_20250716.py
+++ Py_20250716.py
@@ -2,3 +2,5 @@ from datetime import datetime
 print(f"This is a demo file for git/GitHub at: {datetime.now()}")
 
 print("For Second Change")
+
+print("Delete Py_testing.py, and Modify Py_20250716.py for Third push")
